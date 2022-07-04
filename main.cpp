ff32a2618a5516cc8d3ee722c0f9e9a3e7c8a76a
Harrison <ht031801@byu.edu>
2022-07-03 22:26:26 -0600

    Project 3

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <fstream>
+
+using namespace std;
+
+int main(int arcg, char* argv[]) {
+
+    string fileName = argv[];
+
+    
+
+
+
+
+
+
+    return 0;
+}

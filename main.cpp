7c35c2a312e620132a2825ed3a25a02ec0a094ad
Harrison <ht031801@byu.edu>
2022-07-03 23:51:20 -0600

    Project 3

--- main.cpp
+++ main.cpp
@@ -10,6 +10,36 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
 
+    string fileName = argv[1];
+
+    vector <string> tokens;
+    set <string> uniques;
+
+    string nextLine;
+    string tempWord;
+
+    ifstream input(fileName);
+
+    while (!input.eof()) {
+        getline(input, nextLine);
+
+        for (int i = 0; i << nextLine.size(); ++i) {
+
+            if (!isalpha(nextLine.at(i))) {
+                if (!isspace(nextLine.at(i))) {
+                    nextLine.erase(i);
+
+                }
+
+                tempWord = nextLine.substr(0, i);
+                
+            }
+        }
+    }
+
+    input.close();
+
+
 
 
 

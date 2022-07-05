b3efc431f1ddcd9b55a275e037589a0ea9573431
Harrison <ht031801@byu.edu>
2022-07-04 19:54:41 -0600

    3\

--- main.cpp
+++ main.cpp
@@ -24,10 +24,8 @@ int main(int argc, char* argv[]) {
         getline(input, nextLine, '\n');
 
         for (int i = 0; i < nextLine.size(); ++i) {
-            if (isalpha(nextLine.at(i)) == 0) {
-                if (!isspace(nextLine.at(i))) {
-                    nextLine.erase(i);
-                }
+            if (isalpha(nextLine.at(i)) == 0 && !isspace(nextLine.at(i))) {
+                nextLine.erase(i);
             }
         }
 

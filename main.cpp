f15bf8265c6ca69d51a74c150f66aecc49eeb7e7
Harrison <ht031801@byu.edu>
2022-07-04 19:58:11 -0600

    3

--- main.cpp
+++ main.cpp
@@ -21,11 +21,12 @@ int main(int argc, char* argv[]) {
     ifstream input(fileName);
 
     while (!input.eof()) {
-        getline(input, nextLine, '\n');
+        getline(input, nextLine);
 
         for (int i = 0; i < nextLine.size(); ++i) {
             if (isalpha(nextLine.at(i)) == 0 && !isspace(nextLine.at(i))) {
                 nextLine.erase(i);
+                continue;
             }
         }
 

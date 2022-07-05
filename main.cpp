f866e9e70558e0e14e50e437fb7f7610102113d9
Harrison <ht031801@byu.edu>
2022-07-04 19:32:02 -0600

    Project 3

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]) {
 
         for (int i = 0; i < nextLine.size(); ++i) {
             if (isalpha(nextLine.at(i)) == 0) {
-                if (nextLine.at(i) == ' ') {
+                if (nextLine.at(i) != ' ') {
                     nextLine.erase(i);
                 }
             }

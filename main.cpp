295a80b16893698b358911cc133f1702a3463632
Harrison <ht031801@byu.edu>
2022-07-04 19:41:42 -0600

    3

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]) {
 
         for (int i = 0; i < nextLine.size(); ++i) {
             if (isalpha(nextLine.at(i)) == 0) {
-                if (nextLine.at(i) != ' ') {
+                if (isspace(nextLine.at(i)) == false) {
                     nextLine.erase(i);
                 }
             }

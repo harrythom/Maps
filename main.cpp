71c7035c561708c8933774bc4444926f4c28aff4
Harrison <ht031801@byu.edu>
2022-07-06 21:20:16 -0600

    3

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <map>
@@ -13,53 +14,43 @@ int main(int argc, char* argv[]) {
 
     vector <string> tokens;
     set <string> uniques;
-    map <string, string> wordMap;
-
+    
     string nextLine;
-    string tempWord;
 
     ifstream input(fileName);
 
-    while (!input.eof()) {
-        getline(input, nextLine);
+    if (input.is_open()) {
 
-        for (int i = 0; i < nextLine.size(); ++i) {
-            if (ispunct(nextLine.at(i)) != 0) {
-                nextLine.erase(i, 1);
+        while (!input.eof()) {
+            getline(input, nextLine);
+
+            istringstream iss(nextLine);
+            string token;
+
+            while (iss >> token) {
+                string noPunct = "";
+
+                for (auto &c : token) {
+                    if (isalpha(c)) {
+                        noPunct += c;
+                    }
+                }
+                tokens.push_back(noPunct);
+                uniques.insert(noPunct);
             }
         }
-
-        while (nextLine.find(" ") != string::npos) {
-            tempWord = nextLine.substr(0, nextLine.find(" "));
-            nextLine.erase(0, (nextLine.find(" ") + 1));
-
-            tokens.push_back(tempWord);
-            uniques.insert(tempWord);
-        }
-
-        tokens.push_back(nextLine);
-        uniques.insert(nextLine);
+        input.close();
+    }
+    else {
+        cout << "Could not open the specified file.";
+        return 0;
     }
 
-    input.close();
 
     cout << "Num words: " << tokens.size() << endl;
     cout << "Num unique words: " << uniques.size() << endl;
 
-    ofstream output("Words.txt");
-
-    for (int i = 0; i < tokens.size(); ++i) {
-        output << tokens.at(i) << endl;
-    }
-
-    output.close();
-
-    // string last;
-
-    // for (int i = 0; i < tokens.size(); ++i) {
-    //     wordMap[last] = tokens.at(i);
-    //     last = tokens.at(i);
-    // }
+    
 
     
     return 0;
